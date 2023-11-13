f400439ba4ff14f425e2f70ce7d73ab45c8ad2fa
Vivian White <vivian.white@mail.utoronto.ca>
2023-11-13 15:45:13 -0500

    Added minimal code to interfaces in modify_events to get an initial outlook of the ModifyEvents application business rules.

--- src/use_case/modify_events/ModifyUserDataAccessInterface.java
+++ src/use_case/modify_events/ModifyUserDataAccessInterface.java
@@ -1,4 +1,10 @@
 package use_case.modify_events;
+import entity.event.Event;
 
 public interface ModifyUserDataAccessInterface {
+
+    boolean existsById(String id);
+
+    void save(Event event);
+
 }
