771dacd135c57c9f2b74708d42c403ec6a1739cd
Shubhkarman Pruthi <shubhs2004@hotmail.com>
2023-11-13 16:07:39 -0500

    created SignupController.java and SignupState.java

---
+++ src/interface_adapter/signup/SignupController.java
@@ -0,0 +1,22 @@
+package interface_adapter.signup;
+
+import use_case.signup.SignupInputBoundary;
+import use_case.signup.SignupInputData;
+
+import java.util.List;
+
+public class SignupController {
+    final SignupInputBoundary userSignupUseCaseInteractor;
+    public SignupController(SignupInputBoundary userSignupUseCaseInteractor) {
+        this.userSignupUseCaseInteractor = userSignupUseCaseInteractor;
+    }
+
+    public void execute(String username, String password1, String password2,
+                        String firstName, String lastName, List<String> tags) {
+
+        SignupInputData signupInputData = new SignupInputData(
+                username, password1, password2, firstName, lastName, tags);
+
+        userSignupUseCaseInteractor.execute(signupInputData);
+    }
+}
