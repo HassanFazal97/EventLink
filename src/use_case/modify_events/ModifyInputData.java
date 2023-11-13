9fafa8a9a91899a21db79e630f5c0bbfd77735c5
Vivian White <vivian.white@mail.utoronto.ca>
2023-11-13 15:53:26 -0500

    Implemented rough implementation of input and output data for ModifyEvents use case.

--- src/use_case/modify_events/ModifyInputData.java
+++ src/use_case/modify_events/ModifyInputData.java
@@ -1,4 +1,38 @@
 package use_case.modify_events;
 
 public class ModifyInputData {
+
+    private final String name;
+    private final String start;
+    private final String end;
+    private final String currency;
+    private final String summary;
+    private final Boolean isPrivate;
+
+    public ModifyInputData(String name, String start, String end, String currency, String summary, Boolean isPrivate){
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.currency = currency;
+        this.summary = summary;
+        this.isPrivate = isPrivate;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getStart() {
+        return start;
+    }
+    public String getEnd() {
+        return end;
+    }
+    public String getCurrency(){
+        return currency;
+    }
+    public String getSummary(){
+        return summary;
+    }
+    public Boolean getIsPrivate(){ return isPrivate; }
+
 }
