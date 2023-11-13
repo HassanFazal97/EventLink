4fc6adb4c9ac11fac4d13963931ca57c52b4f5c0
Shubhkarman Pruthi <shubhs2004@hotmail.com>
2023-11-13 16:57:04 -0500

    added a events parameter that keeps track of all of user's registered events.
    Updated Signup files whereever necessary to account for new parameter of user.

--- src/entity/user/UserFactory.java
+++ src/entity/user/UserFactory.java
@@ -1,8 +1,9 @@
 package entity.user;
 
+import entity.event.Event;
 import entity.user.User;
 
 import java.util.List;
 public interface UserFactory {
-    User create(String firstName, String lastName, String username, String password, List<String> tags);
+    User create(String firstName, String lastName, String username, String password, List<Event> events, List<String> tags);
 }
