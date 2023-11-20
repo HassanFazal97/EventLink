f839f754a9a2edbd6c2fdc0cd0f486c608d604ee
becca776 <rrjkong@gmail.com>
2023-11-20 16:22:07 -0500

    updated interactor and data access interface to save registered events to the list of common user's events.

--- src/main/java/com/example/eventlink/use_case/register_for_event/RegisterForEventDataAccessInterface.java
+++ src/main/java/com/example/eventlink/use_case/register_for_event/RegisterForEventDataAccessInterface.java
@@ -1,4 +1,9 @@
 package com.example.eventlink.use_case.register_for_event;
 
+import com.example.eventlink.entity.user.CommonUser;
+
 public interface RegisterForEventDataAccessInterface {
+    void UpdateUser(CommonUser commonUser);
+
+    CommonUser getUsername(String username);
 }
--- src/main/java/com/example/eventlink/use_case/register_for_event/RegisterForEventInteractor.java
+++ src/main/java/com/example/eventlink/use_case/register_for_event/RegisterForEventInteractor.java
@@ -2,6 +2,7 @@ package com.example.eventlink.use_case.register_for_event;
 
 import com.example.eventlink.entity.event.Event;
 import com.example.eventlink.entity.event.EventFactory;
+import com.example.eventlink.entity.user.CommonUser;
 import com.example.eventlink.entity.user.UserFactory;
 
 public class RegisterForEventInteractor implements RegisterForEventInputBoundary {
@@ -19,6 +20,7 @@ public class RegisterForEventInteractor implements RegisterForEventInputBoundary
         this.userFactory = userFactory;
         this.eventFactory = eventFactory;
     }
+
     @Override
     public void execute(RegisterForEventInputData registerForEventInputData) {
         Event event = registerForEventInputData.getEvent();
@@ -27,10 +29,23 @@ public class RegisterForEventInteractor implements RegisterForEventInputBoundary
         String username = registerForEventInputData.getUsername();
         String paymentFee = registerForEventInputData.getPaymentFee();
 
-        // Need to actually link events to users and users to event
+        CommonUser commonUser = eventDataAccessObject.getUsername(username);
 
-        RegisterForEventOutputData registerForEventOutputData = new RegisterForEventOutputData();
-        eventRegistrationPresenter.prepareSuccessView(registerForEventOutputData);
+        // Check if the user exists
+        if (commonUser != null) {
+            // Add the event to the user's list of events commonUser.getEvents().add(event);
+            commonUser.getEvents().add(event);
+
+            // Update the user in the data access object
+            eventDataAccessObject.UpdateUser(commonUser);
+            RegisterForEventOutputData registerForEventOutputData = new RegisterForEventOutputData();
+            eventRegistrationPresenter.prepareSuccessView(registerForEventOutputData);
+
+        } else {
+            // User not found, handle accordingly by prepare a fail view
+            eventRegistrationPresenter.prepareFailView("User not found");
+
+        }
     }
 }
 
