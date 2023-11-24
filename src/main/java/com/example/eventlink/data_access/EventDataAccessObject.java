3b91d32ff41f7ce4cc2dd8ac34b74709cc250998
Vivian White <vivian.white@mail.utoronto.ca>
2023-11-24 13:06:10 -0500

    Modified EventDataAccessObject to generate and save events using the ID generated on Eventbrite. WARNING: I changed EventFactory and CommonEventFactory to do so. This might break code that previously used this interface. DO NOT CONTINUE USING THIS INTERFACE DIRECTLY, instead, use the DAO to save and generate events for you.

--- src/main/java/com/example/eventlink/data_access/EventDataAccessObject.java
+++ src/main/java/com/example/eventlink/data_access/EventDataAccessObject.java
@@ -1,10 +1,8 @@
 package com.example.eventlink.data_access;
 
-import com.example.eventlink.entity.event.Event;
+import com.example.eventlink.entity.event.*;
 import com.example.eventlink.use_case.view_event.ViewEventDataAccessInterface;
-import com.example.eventlink.entity.event.CommonEvent;
 import com.example.eventlink.entity.event.Event;
-import com.example.eventlink.entity.event.EventFactory;
 import com.example.eventlink.use_case.modify_events.ModifyEventDataAccessInterface;
 import com.example.eventlink.use_case.create_events.CreateEventDataAccessInterface;
 import com.example.eventlink.use_case.register_for_event.RegisterForEventDataAccessInterface;
@@ -24,6 +22,7 @@ public class EventDataAccessObject implements ViewEventDataAccessInterface,
         ModifyEventDataAccessInterface, CreateEventDataAccessInterface,
         RegisterForEventDataAccessInterface {
 
+    //TODO: Change interfaces to allow for proper ID generation
     private final File dataFile;
     private final Map<String, Integer> headers = new LinkedHashMap<String, Integer>();
     private final Map<String, Event> events = new HashMap<String, Event>();
@@ -42,30 +41,30 @@ public class EventDataAccessObject implements ViewEventDataAccessInterface,
 
         if (dataFile.length() == 0) {
             saveToFile();
-//        } else {
-//
-//            try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
-//                String header = reader.readLine();
-//
-//                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
-//                assert header.equals("name,start,end,currency,summary,isPrivate,id");
-//
-//                String row;
-//                while ((row = reader.readLine()) != null) {
-//                    String[] col = row.split(",");
-//                    String name = String.valueOf(col[0]);
-//                    String start = String.valueOf(col[1]);
-//                    String end = String.valueOf(col[2]);
-//                    String currency = String.valueOf(col[3]);
-//                    String summary = String.valueOf(col[4]);
-//                    String privacy = String.valueOf(col[5]);
-//                    Boolean isPrivate = privacy.equals("true");
-//                    String id = String.valueOf(col[6]);
-//
-//                    Event event = eventFactory.create(name, start, end, currency, summary, isPrivate);
-//                    events.put(event.getID(), event);
-//                }
-//            }
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
+                String header = reader.readLine();
+
+                // TODO: clean this up by creating a new Exception subclass and handling it in the UI.
+                assert header.equals("name,start,end,currency,summary,isPrivate,id");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String name = String.valueOf(col[0]);
+                    String start = String.valueOf(col[1]);
+                    String end = String.valueOf(col[2]);
+                    String currency = String.valueOf(col[3]);
+                    String summary = String.valueOf(col[4]);
+                    String privacy = String.valueOf(col[5]);
+                    Boolean isPrivate = privacy.equals("true");
+                    String id = String.valueOf(col[6]);
+
+                    Event event = eventFactory.create(id, name, start, end, currency, summary, isPrivate);
+                    events.put(id, event);
+                }
+            }
         }
     }
 
@@ -101,14 +100,42 @@ public class EventDataAccessObject implements ViewEventDataAccessInterface,
     }
 
     @Override
-    public void save(Event event) {
-        events.put(event.getID(), event);
+    //TODO: I think this and below methods need their API calls modified
+    public String save(String name, String start, String end, String currency, String summary, Boolean isPrivate) {
+        Client client = ClientBuilder.newClient();
+        Entity payload = Entity.json("{  \"event\": {    \"name\": {      \"html\": \"&#60;p&#62;" + name + "&#60;/p&#62;\"    },    \"description\": {      \"html\": \"&#60;p&#62;Some text&#60;/p&#62;\"    },    \"start\": {      \"timezone\": \"UTC\",      \"utc\": \"2018-05-12T02:00:00Z\"    },    \"end\": {      \"timezone\": \"UTC\",      \"utc\": \"2018-05-12T02:00:00Z\"    },    \"currency\": \"USD\",    \"online_event\": false,    \"organizer_id\": \"\",    \"listed\": false,    \"shareable\": false,    \"invite_only\": false,    \"show_remaining\": true,    \"password\": \"12345\",    \"capacity\": 100,    \"is_reserved_seating\": true,    \"is_series\": true,    \"show_pick_a_seat\": true,    \"show_seatmap_thumbnail\": true,    \"show_colors_in_seatmap_thumbnail\": true,    \"locale\": \"de_AT\"  }}");
+        Response response = client.target("https://www.eventbriteapi.com/v3/organizations/{1861001665463}/events/")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer V62FODWQELD5JCBTLNQC")
+                .post(payload);
+
+        System.out.println("status: " + response.getStatus());
+        System.out.println("headers: " + response.getHeaders());
+        System.out.println("body:" + response.readEntity(String.class));
+
+        //TODO: i have no idea if this is valid code or not
+        String id = response.getHeaderString("id");
+
+        Event event = eventFactory.create(id, name, start, end, currency, summary, isPrivate);
+
+        events.put(id, event);
         this.saveToFile();
+        return id;
     }
 
     @Override
-    public void remove(Event event) {
-        events.remove(event.getID());
+    public void remove(String id) {
+        Client client = ClientBuilder.newClient();
+        Response response = client.target("https://www.eventbriteapi.com/v3/events/" + event.getID() + "/")
+                .request(MediaType.TEXT_PLAIN_TYPE)
+                .header("Authorization", "Bearer V62FODWQELD5JCBTLNQC")
+                .delete();
+
+        System.out.println("status: " + response.getStatus());
+        System.out.println("headers: " + response.getHeaders());
+        System.out.println("body:" + response.readEntity(String.class));
+
+        events.remove(id);
         this.saveToFile();
     }
 
@@ -131,6 +158,9 @@ public class EventDataAccessObject implements ViewEventDataAccessInterface,
         event.setCurrency(currency);
         event.setSummary(summary);
         event.setIsPrivate(isPrivate);
+
+        this.saveToFile();
+
         return event;
     }
 
