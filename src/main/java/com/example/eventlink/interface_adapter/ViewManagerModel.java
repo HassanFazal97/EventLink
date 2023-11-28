b2aa2d536f528f76b04bd83f95b7360cece22a39
Shubhkarman Pruthi <likeaboss10211@gmail.com>
2023-11-27 20:18:20 -0500

    implemented shell of ViewManagerModel
    Need to implement firePropertyChanged and AddPropertyChangeListener according to view

--- src/main/java/com/example/eventlink/interface_adapter/ViewManagerModel.java
+++ src/main/java/com/example/eventlink/interface_adapter/ViewManagerModel.java
@@ -1,4 +1,27 @@
 package com.example.eventlink.interface_adapter;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+//TODO Implement firePropertyChanged() and addPropertyChangeListener
 public class ViewManagerModel {
+    private String activeViewName;
+
+    public String getActiveView() {
+        return activeViewName;
+    }
+
+    public void setActiveView(String activeView) {
+        this.activeViewName = activeView;
+    }
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
