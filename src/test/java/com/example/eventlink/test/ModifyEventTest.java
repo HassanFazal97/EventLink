e813a49e075faa162eb4e84f516b8478a0104a4c
fazal <fazal.hassan@mail.utoronto.ca>
2023-12-01 16:10:03 -0500

    Reworked the modify Events functionality to work with the new implementation of our Events with the new parameters. Also had to modify the use case files for modify event to accept said parameters. Added Tests to ensure modifyEvents works as expected

---
+++ src/test/java/com/example/eventlink/test/ModifyEventTest.java
@@ -0,0 +1,39 @@
+package com.example.eventlink.test;
+
+import com.example.eventlink.data_access.EventDataAccessObject;
+import com.example.eventlink.entity.event.CommonEventFactory;
+import com.example.eventlink.entity.event.Event;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ModifyEventTest {
+
+    private EventDataAccessObject eventDataAccessObject;
+    private Event event;
+
+
+
+//    @Test
+//    public void testEventIdIsNotNull() throws IOException {
+//        eventDataAccessObject = new EventDataAccessObject("src/test.csv", new CommonEventFactory());
+//        Event event = eventDataAccessObject.create("testEvent", "2024-05-12", "02:00", "2024-06-12","02:00","USD", "Testing Event", false);
+//        assertNotNull("Event ID should not be null", event.getID());
+//
+//    }
+
+    @Test
+    public void testEventGetters() throws IOException {
+        eventDataAccessObject = new EventDataAccessObject("src/test.csv", new CommonEventFactory());
+        Event event = eventDataAccessObject.create("testEvent", "2024-05-12", "02:00", "2024-06-12","02:00","USD", "Testing Event", false);
+        String newEvent = eventDataAccessObject.modify(event.getID(), "testModifyEvent", "2025-05-12", "03:00","2025-06-12","03:00","CAD", "Testing Modify Event", true);
+        assertEquals("Name should match", "testModifyEvent", event.getName());
+        assertEquals("Start date should match", "2025-05-12T03:00:00Z", event.getStart());
+        assertEquals("End date should match", "2025-06-12T03:00:00Z", event.getEnd());
+        assertEquals("Currency should match", "CAD", event.getCurrency());
+        assertEquals("Summary should match", "Testing Modify Event", event.getSummary());
+        assertTrue("Private flag should be true", event.getIsPrivate());
+    }
+}
