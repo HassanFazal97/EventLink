82a184f8820b52a6972a5327541ad11e6be1b34f
Vivian White <vivian.white@mail.utoronto.ca>
2023-11-30 20:47:05 -0500

    Implemented the rest of the interface adapter for the ModifyEvent use case.

--- src/main/java/com/example/eventlink/interface_adapter/modify_events/ModifyState.java
+++ src/main/java/com/example/eventlink/interface_adapter/modify_events/ModifyState.java
@@ -3,31 +3,21 @@ package com.example.eventlink.interface_adapter.modify_events;
 public class ModifyState {
 
     private String name = "";
-    private String nameError = null;
     private String start= "";
-    private String startError = null;
     private String end = "";
-    private String endError = null;
     private String currency = "";
-    private String currencyError = null;
     private String summary = "";
-    private String summaryError = null;
     private Boolean isPrivate = false;
-    private Boolean isPrivateError = null;
+    private String eventError = null;
 
     public ModifyState(ModifyState copy){
         this.name = copy.getName();
-        this.nameError = copy.getNameError();
         this.start = copy.getStart();
-        this.startError = copy.getStartError();
         this.end = copy.getEnd();
-        this.endError = copy.getEndError();
         this.currency = copy.getCurrency();
-        this.currencyError = copy.getCurrencyError();
         this.summary = copy.getSummary();
-        this.summaryError = copy.getSummaryError();
         this.isPrivate = copy.getIsPrivate();
-        this.isPrivateError = copy.getIsPrivateError();
+        this.eventError = copy.getEventError();
     }
 
     public ModifyState() {}
@@ -39,14 +29,6 @@ public class ModifyState {
     public void setName(String name) {
         this.name = name;
     }
-    public String getNameError() {
-        return nameError;
-    }
-
-    public void setNameError(String nameError) {
-        this.nameError = nameError;
-    }
-
     public String getStart() {
         return start;
     }
@@ -54,15 +36,6 @@ public class ModifyState {
     public void setStart(String start) {
         this.start = start;
     }
-
-    public String getStartError() {
-        return startError;
-    }
-
-    public void setStartError(String startError) {
-        this.startError = startError;
-    }
-
     public String getEnd() {
         return end;
     }
@@ -70,15 +43,6 @@ public class ModifyState {
     public void setEnd(String end) {
         this.end = end;
     }
-
-    public String getEndError() {
-        return endError;
-    }
-
-    public void setEndError(String endError) {
-        this.endError = endError;
-    }
-
     public String getCurrency() {
         return currency;
     }
@@ -86,15 +50,6 @@ public class ModifyState {
     public void setCurrency(String currency) {
         this.currency = currency;
     }
-
-    public String getCurrencyError() {
-        return currencyError;
-    }
-
-    public void setCurrencyError(String currencyError) {
-        this.currencyError = currencyError;
-    }
-
     public String getSummary() {
         return summary;
     }
@@ -102,15 +57,6 @@ public class ModifyState {
     public void setSummary(String summary) {
         this.summary = summary;
     }
-
-    public String getSummaryError() {
-        return summaryError;
-    }
-
-    public void setSummaryError(String summaryError) {
-        this.summaryError = summaryError;
-    }
-
     public Boolean getIsPrivate() {
         return isPrivate;
     }
@@ -119,13 +65,7 @@ public class ModifyState {
         this.isPrivate = isPrivate;
     }
 
-    public Boolean getIsPrivateError() {
-        return isPrivateError;
-    }
-
-    public void setIsPrivateError(Boolean isPrivateError) {
-        this.isPrivateError = isPrivateError;
-    }
-
+    public String getEventError() { return eventError; }
+    public void setEventError(String eventError) { this.eventError = eventError; }
 
 }
