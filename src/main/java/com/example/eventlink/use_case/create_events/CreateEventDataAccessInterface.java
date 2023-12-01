32b4da8a4d711934b2d50e4db005e11e46672a92
Hong Chen <mkhong.chen@mail.utoronto.ca>
2023-12-01 00:01:20 -0500

    updated eventDAO

--- src/main/java/com/example/eventlink/use_case/create_events/CreateEventDataAccessInterface.java
+++ src/main/java/com/example/eventlink/use_case/create_events/CreateEventDataAccessInterface.java
@@ -4,5 +4,5 @@ import com.example.eventlink.entity.event.Event;
 
 public interface CreateEventDataAccessInterface {
     //TODO: I think this and below methods need their API calls modified
-    Event create(String name, String start, String end, String currency, String summary, Boolean isPrivate);
+    Event create(String name, String startDate, String startTime, String endDate, String endTime, String currency, String summary, Boolean isPrivate);
 }
