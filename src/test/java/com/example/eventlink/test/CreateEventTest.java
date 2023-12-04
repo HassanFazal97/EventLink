98b8bf5b5269f608369826fc8b051b9730c0ce57
Vivian White <vivian.white@mail.utoronto.ca>
2023-12-04 17:14:25 -0500

    Changed CreateEventTest to use TestDataAccessObject instead of EventDataAccessObject to improve runtime

--- src/test/java/com/example/eventlink/test/CreateEventTest.java
+++ src/test/java/com/example/eventlink/test/CreateEventTest.java
@@ -1,9 +1,11 @@
 package com.example.eventlink.test;
 
 import com.example.eventlink.data_access.EventDataAccessObject;
+import com.example.eventlink.data_access.TestDataAccessObject;
 import com.example.eventlink.entity.event.CommonEventFactory;
 import com.example.eventlink.entity.event.Event;
 
+import com.example.eventlink.entity.user.CommonUserFactory;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.IOException;
@@ -11,14 +13,14 @@ import static org.junit.Assert.*;
 
 public class CreateEventTest {
 
-    private EventDataAccessObject eventDataAccessObject;
+    private TestDataAccessObject eventDataAccessObject;
     private Event event;
 
 
 
     @Test
     public void testEventIdIsNotNull() throws IOException {
-        eventDataAccessObject = new EventDataAccessObject("src/test.csv", new CommonEventFactory());
+        eventDataAccessObject = new TestDataAccessObject(new CommonEventFactory(), new CommonUserFactory());
         Event event = eventDataAccessObject.create("testEvent", "2024-05-12", "02:00", "2024-06-12","02:00","USD", "Testing Event", false);
         assertNotNull("Event ID should not be null", event.getID());
 
@@ -26,7 +28,7 @@ public class CreateEventTest {
 
     @Test
     public void testEventGetters() throws IOException {
-        eventDataAccessObject = new EventDataAccessObject("src/test.csv", new CommonEventFactory());
+        eventDataAccessObject = new TestDataAccessObject(new CommonEventFactory(), new CommonUserFactory());
         Event event = eventDataAccessObject.create("testEvent", "2024-05-12", "02:00", "2024-06-12","02:00","USD", "Testing Event", false);
         assertEquals("Name should match", "testEvent", event.getName());
         assertEquals("Start date should match", "2024-05-12 02:00", event.getStart());
