63abbf2d57e8473c5b00c64a8ebb1680ecc803c2
Shubhkarman Pruthi <likeaboss10211@gmail.com>
2023-12-05 17:06:40 -0500

    Added GuestViewTest to test suite.

--- src/test/java/com/example/eventlink/test/RunAllTests.java
+++ src/test/java/com/example/eventlink/test/RunAllTests.java
@@ -1,6 +1,7 @@
 package com.example.eventlink.test;
 
 
+import com.example.eventlink.test.GUI.GuestViewTest;
 import com.example.eventlink.test.GUI.LoginViewTest;
 import com.example.eventlink.test.GUI.SignUpViewTest;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        GuestViewTest.class,
         LoginViewTest.class,
         SignUpViewTest.class,
         CreateEventTest.class,
